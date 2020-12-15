d4ac1a594d2dcb098dbf9dbe5b36a96e4a0f6953
nathan sheryak <nathanraysheryak@gmail.com>
2020-12-15 13:55:15 -0500

    add userSettings and date-fns

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    Header
+    <h1>Hey, {{ userSettings.username }}</h1>
+    <h3>happy {{ date.day }}, {{ date.date }}</h3>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns';
+import userSettings from '../../userSettings.json';
+
 export default {
   name: 'Header',
+  data() {
+    return {
+      userSettings,
+    };
+  },
+  computed: {
+    date() {
+      const today = new Date();
+      const day = format(today, 'iiii');
+      const date = format(today, 'MMMM LL');
+      return { day, date };
+    },
+  },
 };
 </script>
