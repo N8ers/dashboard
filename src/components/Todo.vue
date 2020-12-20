e21bcb944e0cbce8fb612068d588f768ada40d31
nathan sheryak <nathanraysheryak@gmail.com>
2020-12-20 09:23:41 -0500

    todo: edit, add, delete

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="todo-wrapper green-border">
+
     <h3 class="m-10">ToDo</h3>
-    <div v-for="(todo, index) in userSettings.todo" :key="todo.name" class="todo">
+    <button v-if="editMode" @click="editMode = !editMode">Save</button>
+    <button v-if="!editMode" @click="editMode = !editMode">Edit</button>
+
+    <div v-for="(todo, index) in userSettings.todo" :key="todo.index" class="todo">
       <div :class="index % 2 === 0 ? 'even-todo': 'odd-todo'" class="row">
         <input type="checkbox" v-model="todo.completed" />
-        <span class="ml-10">{{ todo.name }}</span>
+        <input v-if="editMode" class="ml-10" type="text" v-model="todo.name" />
+        <span v-if="!editMode" class="ml-10">{{ todo.name }}</span>
+        <button v-if="editMode" @click="removeTodo(index)">remove</button>
       </div>
     </div>
+
+    <div>
+      <input type="text" v-model="newTodo" />
+      <button @click="addTodo">add</button>
+    </div>
+
   </div>
 </template>
 
@@ -18,7 +30,18 @@ export default {
   data() {
     return {
       userSettings,
+      editMode: false,
+      newTodo: null,
     };
+  },
+  methods: {
+    addTodo() {
+      userSettings.todo.push({ completed: false, name: this.newTodo });
+      this.newTodo = null;
+    },
+    removeTodo(todoIndex) {
+      userSettings.todo.splice(todoIndex, 1);
+    },
   },
 };
 </script>
