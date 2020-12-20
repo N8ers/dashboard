fc5f6226259cd78d25d8a62118f3aa3db92527e2
nathan sheryak <nathanraysheryak@gmail.com>
2020-12-20 12:47:49 -0500

    setup basic vuex for settings page

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -10,9 +10,9 @@
     <fieldset>
       <button>update</button>
       <label>User name:</label>
-      <input type="text" v-model="userSettings.username" />
+      <input type="text" v-model="userSettings.user.username" />
       <label>Location:</label>
-      <input type="text" v-model="userSettings.location.name" />
+      <input type="text" v-model="userSettings.user.location.name" />
       <input type="text" ref="search" v-model="location" />
     </fieldset>
   </div>
@@ -34,18 +34,20 @@ export default {
       const autocomplete = new window.google.maps.places.Autocomplete(this.$refs.search);
       autocomplete.addListener('place_changed', () => {
         const place = autocomplete.getPlace();
-        this.location = {
+        const updatedLocation = {
           city: place.address_components[0].long_name,
           state: place.address_components[2].long_name,
           country: place.address_components[3].long_name,
           lat: place.geometry.location.lat(),
           lng: place.geometry.location.lat(),
         };
+        this.$store.dispatch('updateLocation', updatedLocation);
       });
     },
   },
   mounted() {
     window.checkAndAttachMapScript(this.initLocationSearch);
+    this.$store.dispatch('getUser');
   },
 };
 </script>
